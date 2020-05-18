eb167b5703bfce4ae0d6b4c7243d0332bdebf2ea
Zeinab <zeinab@Zeinabs-MacBook-Air.local>
2020-05-18 18:51:24 -0500

    changes

---
+++ newreactapp/src/webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+
+module.exports = {
+  module: {
+    rules: [
+      { test: /\.txt$/, use: 'raw-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
+};
